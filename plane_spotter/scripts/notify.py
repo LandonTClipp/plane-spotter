02516a8132ca9bd7a55a5ce4871ae11311552023
LandonTClipp <landonclipp@gmail.com>
2022-12-16 19:24:28 -0600

    Updates

--- plane_spotter/scripts/notify.py
+++ plane_spotter/scripts/notify.py
@@ -1,8 +1,22 @@
-import click
+from dataclasses import dataclass
 
-@click.command()
-def main():
-    pass
+import hydra
+from hydra.core.config_store import ConfigStore
+from omegaconf import MISSING, OmegaConf
+
+@dataclass
+class Config:
+    adsb_exchange_key: str
+    adsb_exchange_hostname: str = "adsbexchange-com1.p.rapidapi.com"
+
+cs = ConfigStore.instance()
+cs.store(name="notify_schema", node=Config)
+
+
+@hydra.main(version_base=None, config_path=".", config_name="notify")
+def notify(cfg: Config) -> None:
+    print(OmegaConf.to_yaml(cfg))
+
 
 if __name__ == "__main__":
-    main()
+    notify()
